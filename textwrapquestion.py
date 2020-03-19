308e8e2558c119a9ec65f7e3fed3837a855602c5
Tim Newton <timothy.newton.cma13@gmail.com>
2020-03-19 08:18:00 -0400

    save 2

---
+++ textwrapquestion.py
@@ -0,0 +1,17 @@
+test = "ABCDEFGHIJKLIMNOQRSTUVWXYZ"
+
+def wrap(s, max_w):
+    st = ''
+    for i in range(0, len(s), max_w):
+        st += s[i:i+max_w]
+        if i != len(s)-2:
+            st += '\n'
+        #print(s[i:i+max_width])
+    return st
+
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)       
+
