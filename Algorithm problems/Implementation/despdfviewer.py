3ac0c8658a8fbe55aa9df1d0436c1fe87f8a779c
Tim Newton <timothy.newton.cma13@gmail.com>
2020-05-20 15:44:51 -0400

    add new implementation problems

---
+++ Algorithm problems/Implementation/despdfviewer.py
@@ -0,0 +1,20 @@
+def designerPdfViewer(h: list, word: str):
+    base = len(word)
+    abc = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split()
+    pair = dict(zip(abc, h))
+    height = 1
+    for letter in word:
+        if pair[letter] > height:
+            height = pair[letter]
+    return base*height
+
+#abc = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'
+lineIn = '1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5'
+test = list(map(int, lineIn.rstrip().split()))
+#abc = abc.split()
+#print(abc)
+#print(test)
+#combo = dict(zip(abc, test))
+#print(combo['b'])
+
+print(designerPdfViewer(test, 'abc'))
---
+++ Algorithm problems/Implementation/utopiantree.py
@@ -0,0 +1,10 @@
+def utopianTree(n):
+    height = 1
+    for i in range(n):
+        if i%2 == 1:
+            height += 1
+        else:
+            height = height * 2
+    return height
+
+print(utopianTree(5))
