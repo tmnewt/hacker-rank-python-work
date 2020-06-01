58b61bc5100b4bb033c3a46efcb17d3215dd9c98
Tim Newton <timothy.newton.cma13@gmail.com>
2020-06-01 14:35:59 -0400

    fix bad indentation...

--- Algorithm problems/Sorting/InsertionSort.py
+++ Algorithm problems/Sorting/InsertionSort.py
@@ -48,8 +48,8 @@ def insertionSortHR(l):
         j = i-1
         key = l[i]
         while (j >= 0) and (l[j] > key):
-           l[j+1] = l[j]
-           j -= 1
+            l[j+1] = l[j]
+            j -= 1
         l[j+1] = key
     return l
 
