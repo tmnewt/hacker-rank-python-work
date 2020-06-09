270a672d78020e3e4a34447e9f41c371f210b6a5
Tim Newton <timothy.newton.cma13@gmail.com>
2020-06-09 10:52:01 -0500

    add test large test files.

--- Algorithm problems/Sorting/tests/test_generators/sortSpeedGen.py
+++ Algorithm problems/Sorting/tests/test_generators/sortSpeedGen.py
@@ -1,4 +1,15 @@
 import os 
 import numpy as np
 
+os.chdir('Algorithm problems\\Sorting\\tests')
+
 # create huge pool of ints for testing sorting speed
+
+spam = list(np.random.choice(range(-10000000, 10000000), 200000, replace=False))
+
+spam_text = ' '.join(map(str, spam))
+fp = open('large_random_ints_unique_2', 'w')
+
+fp.write(spam_text)
+fp.close()
+print('done')
