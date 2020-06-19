76ce30ba10f18ba30fe1c11b84ba3d329905f836
Tim Newton <timothy.newton.cma13@gmail.com>
2020-06-19 17:46:40 -0500

    simple string solutions

---
+++ python problems/string problems/findAString.py
@@ -0,0 +1,17 @@
+test = 'aracecarisintheracedrivenbyaracerwhomightwintheracecarcup'
+st = 'race'
+
+def count_substring(s, sub):
+    i = 0
+    count = 0
+    while i != -1:
+        spot = s.find(sub, i)
+        if spot != -1: 
+            i = spot+1
+            count += 1
+        else:
+            i = -1
+    return count
+
+print(count_substring(test, st))
+
---
+++ python problems/string problems/stringValidators.py
@@ -0,0 +1,32 @@
+def stringValidator(s: str):
+    s = list(s)
+    # has
+    alphaNum = False; alphabetical = False; digits = False
+    lowercase = False; uppercase = False
+    for i in s:
+        if (alphaNum == True) and (alphabetical == True) and (
+            digits == True) and (lowercase == True) and (
+            uppercase == True):
+            break
+        if i.isalnum():
+            alphaNum = True
+        if i.isalpha():
+            alphabetical = True
+        if i.isdigit():
+            digits = True
+        if i.islower():
+            lowercase = True
+        if i.isupper():
+            uppercase = True
+        
+    print(alphaNum)
+    print(alphabetical)
+    print(digits)
+    print(lowercase)
+    print(uppercase)
+
+test = 'qA2'
+stringValidator(test)
+    
+
+
