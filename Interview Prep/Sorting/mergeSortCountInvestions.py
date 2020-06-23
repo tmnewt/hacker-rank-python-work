2baa27e023bce6d702476ff23711e7c1ece8c90c
Tim Newton <timothy.newton.cma13@gmail.com>
2020-06-23 15:10:02 -0500

    add 2 interview prep probs to complete

---
+++ Interview Prep/Sorting/mergeSortCountInvestions.py
@@ -0,0 +1,3 @@
+# https://www.hackerrank.com/challenges/ctci-merge-sort/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+
+# incomplete
---
+++ Interview Prep/String Manipulation/alternatingCharacters.py
@@ -0,0 +1,3 @@
+# https://www.hackerrank.com/challenges/alternating-characters/problem?h_r=internal-search
+
+# incomplete
