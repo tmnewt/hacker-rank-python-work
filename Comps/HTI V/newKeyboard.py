7a3686119d47dfdd8e9d2255bf6f4ca045b75a95
Timothy Newton <timothy.newton.cma13@gmail.com>
2020-06-26 15:10:42 -0400

    add newKeyBoard problem

---
+++ Comps/HTI V/newKeyboard.py
@@ -0,0 +1,68 @@
+# given a string;
+
+import timeit
+
+
+# within string:
+# '<' represents hitting the home button
+# '>' represents hitting the end key
+# '*' represents hitting the backspace
+# '#' represents hitting the NumLock key
+
+def receivedText(s):
+    result = []
+    digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    numlock = True
+    home = False  # true if the home button has been hit. stays true until end is hit
+    appendix = 0
+    for i in s:
+        if i == '#':
+            numlock = not numlock
+            print(f'numlock is now {numlock}')
+
+        elif i == '<':
+            print('move to front')
+            home = True
+            appendix = 0
+            
+        elif i == '>':
+            print('move to back')
+            home = False
+            
+        elif i == '*':
+            try:
+                if home:
+                    if appendix == 0: pass
+                    else:
+                        result.pop(appendix-1)
+                        appendix -= 1
+                else:
+                    print('deleting from end')
+                    result.pop()
+            except: pass
+
+        elif (numlock == True) and (i in digits):
+            if home:
+                print(f'appending digit to {appendix}')
+                result.insert(appendix, i)
+                appendix += 1
+            else:             
+                print(f'appending digit to end')
+                result.append(i)
+
+        elif i not in digits:
+            if home:
+                result.insert(appendix, i)
+                appendix += 1
+            else:
+                result.append(i)
+
+        print(''.join(result))
+    
+    return ''.join(result)
+            
+print(receivedText('He*<LL>0_>WO<*L**>D<...<#9854j*<abc<d9H#9>a'))
+
+
+
+        
