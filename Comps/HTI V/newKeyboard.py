770d6fad6ce1abdbbbab6526514c280223e1f4f7
Timothy Newton <timothy.newton.cma13@gmail.com>
2020-06-28 19:15:08 -0400

    finish newKeyboard solution

--- Comps/HTI V/newKeyboard.py
+++ Comps/HTI V/newKeyboard.py
@@ -179,7 +179,7 @@ def receivedText(s: str):
                             result = result[appendix:]
                             appendix -= 1 
                         else:
-                            temp = result[:appendix]
+                            temp = result[:appendix-1]
                             result = temp + result[appendix:]
                             appendix -= 1
                                             
@@ -194,8 +194,8 @@ def receivedText(s: str):
                     result = i + result
                     appendix += 1
                 else:
-                    temp = result[:appendix] + i
-                    result = temp + result[appendix:]
+                    
+                    result = result[:appendix] + i + result[appendix:]
                     appendix += 1
             else:             
                 result = result + i
@@ -279,6 +279,209 @@ def receivedText(s: str):
 #''', number= 1))
 #
 
+eggs = 'start<a<#9bZa*>>>>7737<<<2749###<######205xxw***<***j3gj2'
+a = receivedTextOld(eggs)
+b = receivedText(eggs)
+
+print(a == b)
+print(a)
+print(b)
+
+
+#print(timeit.timeit('''
+#
+#def receivedText(s: str):
+#    result = ''
+#    digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+#    numlock = True
+#    home = False  # true if the home button has been hit. stays true until end is hit
+#    appendix = 0
+#    for i in s:
+#        if i == '#': 
+#            numlock = not numlock
+#
+#        elif i == '<':
+#            home = True
+#            appendix = 0
+#            
+#        elif i == '>': 
+#            home = False
+#
+#        elif i == '*':
+#            try:
+#                if home:
+#                    if appendix != 0:
+#                        if appendix == 1:
+#                            result = result[appendix:]
+#                            appendix -= 1 
+#                        else:
+#                            temp = result[:appendix-1]
+#                            result = temp + result[appendix:]
+#                            appendix -= 1
+#                                            
+#                else:
+#                    result = result[:-1]
+#
+#            except: pass
+#
+#        elif (numlock == True) and (i in digits):
+#            if home:
+#                if appendix == 0:
+#                    result = i + result
+#                    appendix += 1
+#                else:
+#                    temp = result[:appendix] + i
+#                    result = temp + result[appendix:]
+#                    appendix += 1
+#            else:             
+#                result = result + i
+#
+#        elif i not in digits:
+#            if home:
+#                if appendix == 0:
+#                    result = i + result
+#                    appendix += 1
+#                else:
+#                    temp = result[:appendix] + i
+#                    result = temp + result[appendix:]
+#                    appendix += 1
+#            else:
+#                result = result + i
+#    return result
+#
+#ham = 'start<a>#9bZa*>>>>7737<<<2749###<######205xxw***<***j3gj2'*20000
+#
+#b = receivedText(ham)
+#
+#''',number=1))
+
+#
+#
+#ham = 'start<a>#9bZa*>>>>7737<<<2749###<######205xxw***<***j3gj2'*j
+##spam = green + ham + 'bc<d9H#9>a'
+#
+#c = receivedTextOld(ham)
+#d = receivedText(ham)
+#print(c == d)
+#print(c)
+#print(d)
+#
+#print(len(ham))
+
+def receivedTextNew(s: str):
+    stacks = []
+    stack = ''
+    last = ''
+    digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    numlock = True
+    home = False
+    for i in s:
+        if i == '#':
+            numlock = not numlock
+
+        elif i == '<':
+            stacks.append(stack)
+            home = True
+            stack = ''
+
+        elif i == '>':
+            stacks.append(stack)
+            home = False
+            stack = ''
+
+        elif i == '*':
+            if home:
+                if stack == '':
+                    pass
+                else:
+                    stack = stack[:-1]
+            else:
+                if last == '':
+                    pass
+                else:
+                    last = last[:-1]
+
+        elif numlock and (i in digits):
+            if home:
+                stack = stack + i
+            else:
+                last = last + i
+
+        elif i not in digits:
+            if home:
+                stack = stack + i
+            else:
+                last = last + i
+
+    stacks.append(stack)
+    result = ''
+    for _ in range(len(stacks)):
+        result = result + stacks.pop()
+
+    return result + last
+
+
+print(receivedTextNew(eggs))
 
 
 
+
+print(timeit.timeit('''
+def receivedTextNew(s: str):
+    stacks = []
+    stack = ''
+    last = ''
+    digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    numlock = True
+    home = False
+    for i in s:
+        if i == '#':
+            numlock = not numlock
+
+        elif i == '<':
+            stacks.append(stack)
+            home = True
+            stack = ''
+
+        elif i == '>':
+            stacks.append(stack)
+            home = False
+            stack = ''
+
+        elif i == '*':
+            if home:
+                if stack == '':
+                    pass
+                else:
+                    stack = stack[:-1]
+            else:
+                if last == '':
+                    pass
+                else:
+                    last = last[:-1]
+
+        elif numlock and (i in digits):
+            if home:
+                stack = stack + i
+            else:
+                last = last + i
+
+        elif i not in digits:
+            if home:
+                stack = stack + i
+            else:
+                last = last + i
+
+    stacks.append(stack)
+    result = ''
+    for _ in range(len(stacks)):
+        result = result + stacks.pop()
+
+    return result + last
+
+
+ham = 'start<a>#9bZa*>>>>7737<<<2749###<######205xxw***<***j3gj2'*20000
+
+b = receivedTextNew(ham)
+
+''', number= 1))
