3abb0163d4a1d3a2312abd6963c86ebf637765f0
Tim Newton <timothy.newton.cma13@gmail.com>
2020-06-28 13:45:09 -0500

    add last comp solution

--- Comps/HTI V/newKeyboard.py
+++ Comps/HTI V/newKeyboard.py
@@ -9,7 +9,7 @@ import timeit
 # '*' represents hitting the backspace
 # '#' represents hitting the NumLock key
 
-def receivedText(s):
+def receivedTextOld(s: str):
     result = []
     digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
     numlock = True
@@ -57,8 +57,162 @@ def receivedText(s):
     
     return ''.join(result)
             
-print(receivedText('He*<LL>0_>WO<*L**>D<...<#9854j*<abc<d9H#9>a'))
 
 
+def receivedTextPrint(s):
+    result = ''
+    digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    numlock = True
+    home = False  # true if the home button has been hit. stays true until end is hit
+    appendix = 0
+    for i in s:
+        print(f'string: {result}')
+        if i == '#': 
+            numlock = not numlock
+            print(f'numlock is now {numlock}')
 
+        elif i == '<':
+            print('move to front')
+            home = True
+            appendix = 0
+            
+        elif i == '>': 
+            home = False
+            print('move to back')
+
+        elif i == '*':
+            try:
+                if home:
+                    if appendix != 0:
+                        if appendix == 1:
+                            print(f'Delete at index {appendix}')
+                            print(f'Before: {result}')
+                            result = result[appendix:]
+                            print(f'After : {result}')
+                            appendix -= 1 
+                        else:
+                            print(f'Delete at index {appendix}')
+                            print(f'Before: {result}')
+                            temp = result[:appendix]
+                            print(f'temp = {temp}')
+                            result = temp + result[appendix:]
+                            print(f'After : {result}')
+                            appendix -= 1
+                                            
+                else:
+                    print('Delete from end')
+                    print(f'Before: {result}')
+                    result = result[:-1]
+                    print(f'After : {result}')
+
+            except: pass
+
+        elif (numlock == True) and (i in digits):
+            if home:
+                if appendix == 0:
+                    print(f'Insert {i} at front')
+                    print(f'Before: {result}')
+                    result = i + result
+                    print(f'After : {result}')
+                    appendix += 1
+                else:
+                    print(f'Insert {i} at {appendix}')
+                    print(f'Before: {result}')
+                    temp = result[:appendix] + i
+                    result = temp + result[appendix:]
+                    print(f'After : {result}')
+                    appendix += 1
+            else:             
+                print(f'Insert {i} to end')
+                print(f'Before: {result}')
+                result = result + i
+                print(f'After : {result}')
+
+        elif i not in digits:
+            if home:
+                if appendix == 0:
+                    print(f'Insert {i} at front')
+                    print(f'Before: {result}')
+                    result = i + result
+                    print(f'After : {result}')
+                    appendix += 1
+                else:
+                    print(f'Insert {i} at {appendix}')
+                    print(f'Before: {result}')
+                    temp = result[:appendix] + i
+                    print(f'temp = {temp}')
+                    result = temp + result[appendix:]
+                    print(f'After : {result}')
+                    appendix += 1
+            else:
+                print(f'Append {i} to end')
+                print(f'Before: {result}')
+                result = result + i
+                print(f'After : {result}')
+
+
+    return result
         
+
+def receivedText(s: str):
+    result = ''
+    digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    numlock = True
+    home = False  # true if the home button has been hit. stays true until end is hit
+    appendix = 0
+    for i in s:
+        if i == '#': 
+            numlock = not numlock
+
+        elif i == '<':
+            home = True
+            appendix = 0
+            
+        elif i == '>': 
+            home = False
+
+        elif i == '*':
+            try:
+                if home:
+                    if appendix != 0:
+                        if appendix == 1:
+                            result = result[appendix:]
+                            appendix -= 1 
+                        else:
+                            temp = result[:appendix]
+                            result = temp + result[appendix:]
+                            appendix -= 1
+                                            
+                else:
+                    result = result[:-1]
+
+            except: pass
+
+        elif (numlock == True) and (i in digits):
+            if home:
+                if appendix == 0:
+                    result = i + result
+                    appendix += 1
+                else:
+                    temp = result[:appendix] + i
+                    result = temp + result[appendix:]
+                    appendix += 1
+            else:             
+                result = result + i
+
+        elif i not in digits:
+            if home:
+                if appendix == 0:
+                    result = i + result
+                    appendix += 1
+                else:
+                    temp = result[:appendix] + i
+                    result = temp + result[appendix:]
+                    appendix += 1
+            else:
+                result = result + i
+    return result
+
+print(receivedTextOld('He*<LL>0_>WO<*L**>D<...<#9854j*<abc<d9H#9>a'))
+print(receivedText('He*<LL>0_>WO<*L**>D<...<#9854j*<abc<d9H#9>a'))
+#print(receivedTextPrint('He*<LL>0_>WO<*L**>D<...<#9854j*<abc<d9H#9>a'))
--- Comps/HTI V/rerouting.py
+++ Comps/HTI V/rerouting.py
@@ -1,8 +1,8 @@
 # if a value equals it's 1-indexed index then the node is referencing itself
 # example: [1, 2, 3, 4]
-# oherwise think of the index as the node and its value as the pointer to the next node (based on 1-indexed)
+# otherwise think of the index as the node and its value as the pointer to the next node (based on 1-indexed)
 # example: [2, 3, 4, 1, 5]
-# is 1(2) -> 2(3) -> 3(4) -> 4(1) with 5 by iteslf
+# is 1(2) -> 2(3) -> 3(4) -> 4(1) with 5 by itself
 # making 1 change here is the minimum amount of work
 # could have 5(n but not 5) or 4(5)
 
@@ -12,6 +12,76 @@
 
 # if none are linked then the minimum is always n - 1 
 
-def getMinConnectionChange(connections):
-    # connections are directional which makes this much easier
-    pass 
+import numpy as np
+
+def getMinConnectionChange(connections: list):
+    if len(connections) == 0:
+        return 0
+    
+    memo = [0] * len(connections)
+    compCount = 0
+    for i in range(len(connections)):        
+        if memo[i] != 0: continue  # end this increment
+
+        startIndex = i + 1 
+        current = i 
+        pointer = connections[current] - 1 
+        
+        while True:
+            if memo[current] == startIndex: # found a component loop or itself.
+                compCount += 1
+                break
+            elif memo[current] != 0: # found an existing component
+                break
+            
+            memo[current] = startIndex
+            current = pointer
+            pointer = connections[current] - 1
+        
+    return compCount-1
+
+def getMinConnectionChangeText(connections: list):
+    # I think the best approach here is to identify components and then the solution will be c-1 
+    # the issue is efficiently 
+    memo = [0] * len(connections)
+    compCount = 0
+    for i in range(len(connections)):
+        print(f'evaluating index {i+1} which contains {connections[i]}')
+        if memo[i] != 0:
+            print(f'{connections[i]} is part of a component') 
+            continue
+        else: print(f'node {i+1}\'s connections hav not been traced. Tracing now.')
+
+        startIndex = i + 1
+        current = i 
+        pointer = connections[current] - 1 
+        while True:
+            print(f'current node: {current+1} points to {connections[current]}')
+            if memo[current] == startIndex:
+                compCount += 1
+                print(f'identified componet. compCount = {compCount}')
+                break
+            elif memo[current] != 0:
+                print('reached an existing component')
+                break
+            memo[current] = startIndex
+            current = pointer
+            pointer = connections[current] - 1
+        
+    return compCount-1
+
+
+
+#index=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,  23]
+test = [2, 1, 4, 5, 5, 7, 8, 9, 10, 9, 15, 15, 15, 15, 15, 15, 15, 15, 15, 21, 22, 20,  14, 24] 
+# comps [ 1 ] [   2  ] [     3,      ] [                4                ] [    5    ]  [4] [6]
+# 5 components in the above test. 
+
+test = list(np.random.randint(1, 300000, 300000))
+print(getMinConnectionChange(test))
+
+blob = [n+1 for n in range(10)]
+print(blob)
+print(getMinConnectionChangeText(blob))
+
+print(getMinConnectionChange([1,2,3]))
