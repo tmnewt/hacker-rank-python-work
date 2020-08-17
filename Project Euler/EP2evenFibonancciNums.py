cc58bb1ad3b7e5a4b86be03a4c4134b85c6261f2
Tim Newton <timothy.newton.cma13@gmail.com>
2020-08-17 09:51:36 -0500

    75 out of 100 solution to Euler #2

--- Project Euler/EP2evenFibonancciNums.py
+++ Project Euler/EP2evenFibonancciNums.py
@@ -1,12 +1,64 @@
 # https://www.hackerrank.com/contests/projecteuler/challenges/euler002/problem
 
-# messing around first
+# use memoization to save time.
+# or use: https://blog.paulhankin.net/fibonacci/
 
-fibs = [1,2]
+# memoization
+def fibonacci(num: int):
+    memo = [0,1]
+    def fibIn(n):
+        if len(memo) > n:  # consider rearranging the conditional statement
+            return memo[n]
+        fibIn(n-1)
+        memo.append(memo[n-1]+memo[n-2])
+        return memo[n]
+    return fibIn(num)
 
-for i in range(200):
-    fibs.append(fibs[-1]+fibs[-2])
+
+# O log n 
+# see: https://blog.paulhankin.net/fibonacci/
+def fibfast(n):
+    n -= 1
+    return (4 << n*(3+n)) // ((4 << 2*n) - (2 << n) - 1) & ((2 << n) - 1)
 
 
-evens = [ i for i in fibs if i%2 == 0]
-print(evens)
+class fiboe:
+    
+    def __init__(self):
+        self.evens = [0,2,8]
+        #self.sums = 
+        #self.highest
+
+    def nextevenfib(self):
+        pass
+        
+
+
+
+
+
+for _ in range(10000):
+
+    n = 4*(10**16)
+
+    evens = []
+    highest = 0
+    i = 1
+    while highest <= n:
+        fib = fibfast(i)
+        if fib > n:
+            break
+        if fib%2 == 0:
+            evens.append(fib)
+        highest = fib
+        i += 1
+
+print('done')
+
+
+# wow, the above is still not fast enough.
+
+# ok, so every 3rd term of the series is even.
+
+# in fact, E(n) = 4 * E(n-1) + E(n-2)
+
