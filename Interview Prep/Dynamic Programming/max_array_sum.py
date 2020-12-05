921752fa7106344f3e68482d97b94845b236e235
Tim Newton <timothy.newton.cma13@gmail.com>
2020-12-05 17:28:50 -0600

    clear up explaination of max_array_sum solution.

--- Interview Prep/Dynamic Programming/max_array_sum.py
+++ Interview Prep/Dynamic Programming/max_array_sum.py
@@ -4,23 +4,20 @@
 # For example:
 #   arr = [-2, 1, 3, -4, 5, 6] has the maximum subset sum of 9.
 
-# The trick here is keep track of the maximum sum value for each element in the array, following these rules:
+# The trick here is keep track of the maximum sum value for each element in the array, 
+#  using a 'shadow' array while following these rules:
 #       
-#   The maximum value for the next element in the array is defined as,  
+#   The maximum value for the next element in the original array is defined as,  
 #
-#   1. The sum of the maximum value from 2 elements prior + the current value,
+#   1. The maximum is the sum of the maximum value from 2 elements prior + the current value,
+#       Or,
 #   2. The current element itself.
 #       Or,
-#   3. If neither 1 or 2 is true the maximum is from an earlier index.
-#
-#
-#
-#   Regardless set the maximum value for each value in a parallel array. 
-#
+#   3. The maximum is from an earlier index.
 #
 #
 # Using the array example above, we get the following
-#       Array                               Maximum array       Max Value           Comments
+#       Array                         Maximum "shadow" array     Max Value           Comments
 #   [ '-2'  1  3  -4  5  6 ]             [ -2 ]                    -2             First 2 indexes get special treatment.  
 #   [  -2  '1' 3  -4  5  6 ]             [ -2  1]                   1             1 replaces -2 as current max
 #   [  -2   1 '3' -4  5  6 ]             [ -2  1  3]                3             3 replaces  1 as current max  (cant be 1 + 3 = 4 because 1 and 3 elements are next to each other)
@@ -30,13 +27,12 @@
 #   Done
 #
 #   Wait, what happened there at the end? How did the program know to use the 3 from earlier? Answer, that 3 is NOT from the original
-#       array, it is actually from the 3 in the maximum array. Remember rule 1 from above. By checking the stored maximum value of the
-#       element that is 2 spots before the current value being evaluated, we can see if adding to the current value will produce a new
-#       maximum.
+#       array, it is actually from the 3 in the "shadow" array. Remember rule 1 from above. By checking the "shadow" maximum value of the
+#       element 2 spots before the current value, we can test if adding it to the current value will produce a new maximum.
 # 
 # 
-# Now, it should be possible to not even need to build a parallel list of maximum values. All it requires is 2 variables. the first
-#   variable holds the current maximum. The second holds the 2nd highest maximum.
+# Now, it should be possible to not even need to build a parallel list of maximum values. All it requires is 2 variables. But I'm
+#   to lazy to think through that 
 # 
 
 def maxSubsetSum(arr):
